f025b41898914b439c960a7734ebd1bba23ae4b8
LuanChico <65927392+LuanChico@users.noreply.github.com>
2023-05-15 22:05:34 -0300

    Projeto Flutter para Squad Scrum;

---
+++ projetos/flutter/squad_scrum/android/app/src/main/kotlin/com/example/squad_scrum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.squad_scrum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projetos/flutter/squad_scrum/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:squad_scrum/Screen/login.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ),
+  );
+}
